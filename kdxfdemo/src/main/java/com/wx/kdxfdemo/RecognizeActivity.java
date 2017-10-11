acd1162019182b9ef8c12920221442bedd78ebd1
王旭 <wx1344394424@163.com>
2017-10-11 20:03:05 +0800

    新建讯飞官方demo

---
+++ kdxfdemo/src/main/java/com/wx/kdxfdemo/RecognizeActivity.java
@@ -0,0 +1,13 @@
+package com.wx.kdxfdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecognizeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recognize);
+    }
+}
